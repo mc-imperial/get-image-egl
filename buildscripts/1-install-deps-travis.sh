f92746fa64d3cd094f359e2d364b5ae72e4c0e72
Hugues Evrard <h.evrard@imperial.ac.uk>
2017-05-19 10:19:01 +0100

    Fix Travis CI with GCC 4.9 for JSON

--- buildscripts/1-install-deps-travis.sh
+++ buildscripts/1-install-deps-travis.sh
@@ -25,14 +25,18 @@ then
   GITHUB_RELEASE_TOOL_ARCH="linux_amd64"
   wget https://github.com/paulthomson/build-angle/releases/download/v-592879ad24e66c7c68c3a06d4e2227630520da36/Linux-x64-Release.zip
   unzip Linux-x64-Release.zip
+
+  # The JSON library requires GCC >= 4.9
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y;
+  sudo apt-get update -qq
+  sudo apt-get install -qq -y g++-4.9
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
+
 fi
 
 cd ..
-
 
 pushd /data/bin
 wget "https://github.com/${GITHUB_RELEASE_TOOL_USER}/github-release/releases/download/${GITHUB_RELEASE_TOOL_VERSION}/github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz"
 tar xf "github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz"
 popd
-
-
